2b6a0f48d3ecd514f53a97336d659e6f3a7fe230
jddev27 <jddev27@gmail.com>
2024-04-15 17:22:42 -0600

    Disabling defualt auto security configuration

--- app/src/main/java/com/example/dishfinder/DishFinderApplication.java
+++ app/src/main/java/com/example/dishfinder/DishFinderApplication.java
@@ -2,9 +2,12 @@ package com.example.dishfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @EnableCaching
 public class DishFinderApplication {
 
