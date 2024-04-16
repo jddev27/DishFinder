5031b8d08c9e2d88b3ec192f16341d6a96805bbf
jddev27 <jddev27@gmail.com>
2024-04-16 07:01:23 -0600

    updating docker file

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM openjdk:21-jdk
 COPY ./app/build/libs/app.jar .
 WORKDIR .
 ENV OPENAI_API_KEY $MY_OPENAI_API_KEY
+ENV APP_API_KEY $MY_APP_API_KEY
 CMD ["java", "-jar","app.jar"]
--- app/src/main/java/com/example/dishfinder/DishFinderApplication.java
+++ app/src/main/java/com/example/dishfinder/DishFinderApplication.java
@@ -2,7 +2,6 @@ package com.example.dishfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
